26f1b65c338bdd8f7815d24adde8f5ae11827217
Paulo Henrique Andrade Alves <80326186+PhAlves23@users.noreply.github.com>
2024-04-06 20:09:11 -0300

    feat: create check in

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { checkIn } from "./routes/check-in";
 
 const app = fastify();
 app.setValidatorCompiler(validatorCompiler);
@@ -16,6 +17,7 @@ app.register(createEvent);
 app.register(registerForEvent);
 app.register(getEvent);
 app.register(getAttendeeBadge);
+app.register(checkIn);
 
 const PORT = 3333;
 app
