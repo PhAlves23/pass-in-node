41124e9c6de18bdabded90330c772b4dd46695b2
Paulo Henrique Andrade Alves <80326186+PhAlves23@users.noreply.github.com>
2024-04-06 10:23:57 -0300

    chore: config prisma and event model

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import fastify from "fastify";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
+
+const app = fastify();
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+
+app.post("/events", async (request, reply) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  });
+
+  const data = createEventSchema.parse(request.body);
+
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      maximumAttendees: data.maximumAttendees,
+      slug: new Date().toISOString(),
+    },
+  });
+
+  return reply.status(201).send({ eventId: event.id });
+});
+
+const PORT = 3333;
+app
+  .listen({
+    port: PORT,
+    host: "0.0.0.0",
+  })
+  .then(() => {
+    console.log(`Server is running on port ${PORT}`);
+  });
