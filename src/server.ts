48bcccd2a358b045604e58f4f8fa892cb5f78559
Paulo Henrique Andrade Alves <80326186+PhAlves23@users.noreply.github.com>
2024-04-06 18:42:54 -0300

    feat: register for event

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,14 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
 
 const app = fastify();
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
+app.register(registerForEvent);
 
 const PORT = 3333;
 app
